573ed072baca7e08f6efd37630e203b44f1be964
Luis <luis.ifh12@gmail.com>
2019-05-09 22:03:47 -0500

    Modo Responsive terminada, animacion de menu y contenido

--- js/scriptt.js
+++ js/scriptt.js
@@ -25,6 +25,29 @@ $(document).ready(function(){
 
     }
   }
+
+  if ($(window).width() < 480) {
+    if (scroll > 100) {
+      $(".menu").css({"background":"#fff"});
+      $(".menu").css({"padding":"0"});
+      $(".menu").addClass("shadow");
+      $(".menu").css({"z-index":"999"});
+      $("nav ul li a").css({"color":"white"});
+      $(".logo").css({"color":"black"});
+   
+  }else{
+    $(".menu").css({"background":"transparent"});
+    $(".menu").css({"padding":"5px 0"});
+    $(".menu").removeClass("shadow");
+    $("nav ul li a").css({"color":"white"});
+    $(".logo").css({"color":"white","margin":"23px"});
+
+
+
+
+
+  }
+}
   
     
   
@@ -32,6 +55,14 @@ $(document).ready(function(){
   
   })
 
+    if($(window).width()<500){
+    $(".menu-icon").on("click",function(){
+      $('nav').slideToggle();
+
+
+    })
+
+    }
 
 
 })
